4a003e28bc8e6b767664540cae13c3a24299a4b7
GaGa-Kim <wn8925@sookmyung.ac.kr>
2021-12-23 00:38:45 +0900

    permitAll

--- src/main/java/com/toyproject/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/toyproject/book/springboot/config/auth/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .authorizeRequests()
                     .antMatchers("/","/css/**","/imges/**","/js/**","/h2-console/**","/api/v1/**").permitAll()
-                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    // antMatchers("/api/v1/**").hasRole(Role.USER.name())
                     .anyRequest().authenticated()
                 .and()
                     .logout().logoutSuccessUrl("/api/v1/posts")    //수정 12/22 6:20
