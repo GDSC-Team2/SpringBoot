009f4138ba35135f62e72acc50ed048a397ffd49
Suah Kang <63537847+rkdtndk99@users.noreply.github.com>
2021-12-24 00:20:08 +0900

    user 조회 api

--- src/main/java/com/toyproject/book/springboot/service/user/UserService.java
+++ src/main/java/com/toyproject/book/springboot/service/user/UserService.java
@@ -1,6 +1,10 @@
 package com.toyproject.book.springboot.service.user;
 
+import com.toyproject.book.springboot.domain.posts.Posts;
+import com.toyproject.book.springboot.domain.user.User;
 import com.toyproject.book.springboot.domain.user.UserRepository;
+import com.toyproject.book.springboot.web.dto.PostsResponseDto;
+import com.toyproject.book.springboot.web.dto.UserResponseDto;
 import com.toyproject.book.springboot.web.dto.UserSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,6 +14,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+
+    public UserResponseDto findById (Long id) {  // 조회
+        User entity = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+ id));
+        return new UserResponseDto(entity);
+    }
 
     @Transactional
     public Long save(UserSaveRequestDto requestDto) {  // 등록
--- src/main/java/com/toyproject/book/springboot/web/UserApiController.java
+++ src/main/java/com/toyproject/book/springboot/web/UserApiController.java
@@ -2,8 +2,11 @@ package com.toyproject.book.springboot.web;
 
 import com.toyproject.book.springboot.domain.user.UserRepository;
 import com.toyproject.book.springboot.service.user.UserService;
+import com.toyproject.book.springboot.web.dto.PostsResponseDto;
+import com.toyproject.book.springboot.web.dto.UserResponseDto;
 import com.toyproject.book.springboot.web.dto.UserSaveRequestDto;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -20,5 +23,12 @@ public class UserApiController {
     @ApiOperation(value = "유저 등록", notes = "유저 등록 API")
     public Long save(@RequestBody UserSaveRequestDto requestDto) {
         return userService.save(requestDto);
+    }
+
+    @GetMapping("/user/{id}")  // 조회 API by ID - 아이디로 하나씩 조회
+    @ApiOperation(value = "사용자 조회 by ID", notes = "글 사용자 API by ID")
+    @ApiImplicitParam(name = "id", value = "사용자 아이디")  // Swagger에 사용하는 파라미터에 대해 설명
+    public UserResponseDto findById (@PathVariable Long id) {
+        return userService.findById(id);
     }
 }
