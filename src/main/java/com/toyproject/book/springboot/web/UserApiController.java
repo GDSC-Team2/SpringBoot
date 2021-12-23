6cf9f69d4320f38d16dbd6db7638815ccd002d10
GaGa-Kim <wn8925@sookmyung.ac.kr>
2021-12-23 23:52:28 +0900

    userapi

--- src/main/java/com/toyproject/book/springboot/web/UserApiController.java
+++ src/main/java/com/toyproject/book/springboot/web/UserApiController.java
@@ -1,6 +1,10 @@
 package com.toyproject.book.springboot.web;
 
+import com.toyproject.book.springboot.domain.user.UserRepository;
+import com.toyproject.book.springboot.service.user.UserService;
+import com.toyproject.book.springboot.web.dto.UserSaveRequestDto;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +13,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1")
 @Api(tags = {"ToyProject API Test"})  // Swagger 최상단 Controller 명칭
 public class UserApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/user")  // 유저 등록 API
+    @ApiOperation(value = "유저 등록", notes = "유저 등록 API")
+    public Long save(@RequestBody UserSaveRequestDto requestDto) {
+        return userService.save(requestDto);
+    }
 
 
 }
