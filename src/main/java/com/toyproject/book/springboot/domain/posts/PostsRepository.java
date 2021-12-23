2641a08700f92afc3bf6180e02d812cd2f613059
GaGa-Kim <wn8925@sookmyung.ac.kr>
2021-12-23 22:01:09 +0900

    작성자 조회

--- src/main/java/com/toyproject/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/toyproject/book/springboot/domain/posts/PostsRepository.java
@@ -1,11 +1,14 @@
 package com.toyproject.book.springboot.domain.posts;
 
+import com.toyproject.book.springboot.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 // Posts 클래스로 데이터베이스에 접근하게 해주는 인터페이스 - DB에 접근하는 영역
 // CRUD 메소드 자동 생성
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    Optional<Posts> findByName(String name);
 }
