a4f2aefc94a154e2ce27aa4a1795ef1e31f5a7ca
Suah Kang <63537847+rkdtndk99@users.noreply.github.com>
2021-12-24 03:57:36 +0900

    userUpdateRequestDto

---
+++ src/main/java/com/toyproject/book/springboot/web/dto/UserUpdateRequestDto.java
@@ -0,0 +1,23 @@
+package com.toyproject.book.springboot.web.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+
+public class UserUpdateRequestDto {
+    @ApiModelProperty(example = "사용자 이름")
+    private String name;
+
+    @ApiModelProperty(example = "사용자 이메일")
+    private String email;
+
+    @ApiModelProperty(example = "사용자 사진")
+    private String picture;
+
+    @Builder
+    public UserUpdateRequestDto(String name, String email, String picture){
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+}
