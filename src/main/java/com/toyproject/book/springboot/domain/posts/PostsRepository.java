19ab980058f4580f8ea960ed69456b26f21f3b73
Suah Kang <63537847+rkdtndk99@users.noreply.github.com>
2021-12-24 03:25:33 +0900

    find post by name

--- src/main/java/com/toyproject/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/toyproject/book/springboot/domain/posts/PostsRepository.java
@@ -2,6 +2,7 @@ package com.toyproject.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 // Posts 클래스로 데이터베이스에 접근하게 해주는 인터페이스 - DB에 접근하는 영역
@@ -9,4 +10,5 @@ import java.util.Optional;
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     Optional<Posts> findByAuthor(String author);   //이메일로 이미 가입한 회원인지 아닌지 확인
 
+    public List<Posts> findByName(String name);
 }
--- src/main/java/com/toyproject/book/springboot/web/PostsApiController.java
+++ src/main/java/com/toyproject/book/springboot/web/PostsApiController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 // API를 요청받는 컨트롤러로 요청을 받는 영역
@@ -47,7 +48,12 @@ public class PostsApiController {
 //    public PostsResponseDto findByAuthor (@PathVariable String author) {
 //        return postsService.findByAuthor(author);
 //    }
-
+    @GetMapping("/posts/{author}")  // 조회 API by author - 아이디로 하나씩 조회
+    @ApiOperation(value = "글 조회 by author", notes = "글 조회 API by author")
+    @ApiImplicitParam(name = "author", value = "작성자")  // Swagger에 사용하는 파라미터에 대해 설명
+    public List<Posts> getPostsByName(Principal principal){
+        return postsRepository.findByName(principal.getName());
+    }
 
 
     @GetMapping("/posts/{id}")  // 조회 API by ID - 아이디로 하나씩 조회
