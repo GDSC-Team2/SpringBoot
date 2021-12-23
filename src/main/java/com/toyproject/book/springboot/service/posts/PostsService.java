c8f46cd15e5854e447395a6be4a9d2ee79e63d93
GaGa-Kim <wn8925@sookmyung.ac.kr>
2021-12-23 23:04:44 +0900

    permitAll

--- src/main/java/com/toyproject/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/toyproject/book/springboot/service/posts/PostsService.java
@@ -25,6 +25,14 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
+    /*
+    public PostsResponseDto findByAuthor (String author) {  // 조회
+        Posts entity = postsRepository.findByAuthorContaining(author).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. author="+ author));
+        return new PostsResponseDto(entity);
+    }
+
+     */
+
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto){  // 수정
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
@@ -37,5 +45,7 @@ public class PostsService {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
         postsRepository.delete(posts);   //JpaRepository에서 제공하는 메소드
     }
+
+
 
 }
--- src/main/java/com/toyproject/book/springboot/web/PostsApiController.java
+++ src/main/java/com/toyproject/book/springboot/web/PostsApiController.java
@@ -10,8 +10,6 @@ import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 
 // API를 요청받는 컨트롤러로 요청을 받는 영역
@@ -41,6 +39,16 @@ public class PostsApiController {
     public List<Posts> getAllPosts() {
         return postsRepository.findAll();
     }
+
+    /*
+    @GetMapping("/posts/my")  // 조회 API - 전체 조회
+    @ApiOperation(value = "내 글 조회", notes = "내 글 조회 API")
+    @ApiImplicitParam(name = "author", value = "작성자")  // Swagger에 사용하는 파라미터에 대해 설명
+    public PostsResponseDto findByAuthorContaining (@PathVariable String author) {
+        return postsService.findByAuthorContaining();
+    }
+
+     */
 
     @GetMapping("/posts/{id}")  // 조회 API by ID - 아이디로 하나씩 조회
     @ApiOperation(value = "글 조회 by ID", notes = "글 조회 API by ID")
