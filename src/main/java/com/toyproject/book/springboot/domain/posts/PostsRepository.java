9e66fe7f54e6959a0b42dbebaf2839a0e8c00b53
Suah Kang <63537847+rkdtndk99@users.noreply.github.com>
2021-12-24 01:40:12 +0900

    posts by me api

--- src/main/java/com/toyproject/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/toyproject/book/springboot/domain/posts/PostsRepository.java
@@ -8,11 +8,7 @@ import java.util.Optional;
 // Posts 클래스로 데이터베이스에 접근하게 해주는 인터페이스 - DB에 접근하는 영역
 // CRUD 메소드 자동 생성
 public interface PostsRepository extends JpaRepository<Posts, Long> {
-<<<<<<< HEAD
-
-    List<Posts> findByAuthorContaining(String author);
     List<Posts> getAllPostsByMe();
-=======
+
     Posts findByAuthor(String author);
->>>>>>> 42b951c127de8ab3684f110f7270a8086d93f996
 }
