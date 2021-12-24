d78b70086e9807f9ef2e3ae4e8cd9a27642ef1bb
GaGa-Kim <wn8925@sookmyung.ac.kr>
2021-12-24 10:33:34 +0900

    posts

--- src/main/java/com/toyproject/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/toyproject/book/springboot/domain/posts/PostsRepository.java
@@ -7,6 +7,6 @@ import java.util.List;
 // Posts 클래스로 데이터베이스에 접근하게 해주는 인터페이스 - DB에 접근하는 영역
 // CRUD 메소드 자동 생성
 public interface PostsRepository extends JpaRepository<Posts, Long> {
-    List<Posts> findByAuthor(String author);
+   // List<Posts> findByAuthor(String author);
 }
 
